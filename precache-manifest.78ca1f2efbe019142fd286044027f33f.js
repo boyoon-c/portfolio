63f8d39ae098016261cb1c5fef0142e02e505ce7
boyoon-c <boyoonchang@gmail.com>
2021-10-05 22:59:10 -0700

    Updates

---
+++ precache-manifest.78ca1f2efbe019142fd286044027f33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75121e401e1839303178e6126223eed",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "75715740634032374953",
+    "url": "/home/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "48335a35aae00248b50c",
+    "url": "/home/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "75715740634032374953",
+    "url": "/home/static/js/2.6014e006.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/home/static/js/2.6014e006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48335a35aae00248b50c",
+    "url": "/home/static/js/main.dafe8cd3.chunk.js"
+  },
+  {
+    "revision": "bac681e6707b590225d4",
+    "url": "/home/static/js/runtime-main.caf7a1cd.js"
+  }
+]);
